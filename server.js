981cfb563991d8821497752ae2027c0f3a74c45d
VVINODA <vinoda05v@gmail.com>
2017-02-03 13:57:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/',function (req,res){
     res.sendFile(path.join(__dirname,'ui','index.html'));
 });
 
-app.get('/article-one',function (req,res){
+app.get('/webpageone-v',function (req,res){
     res.send('web page one-v is served');
 });
 app.get('/ui/madi.png',function (req,res){
