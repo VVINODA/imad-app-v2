8b1d4bb0bbd61cc3a25b4ebabc5ca73c17febb0b
VVINODA <vinoda05v@gmail.com>
2017-02-05 17:06:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,8 +5,7 @@ var element= document.getElementById('main-text'
 
 element.innerHTML='New data using JavaScript';
 
-var foto= document.getElementById('madi');
-foto.onclick= function() 
-{
+var foto = document.getElementById('madi');
+foto.onclick = function() {
     foto.style.marginLeft = '100px';
 };
