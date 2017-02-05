673ba91aad035d07dc684b12a691558bef456ce4
VVINODA <vinoda05v@gmail.com>
2017-02-05 11:59:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ app.get('/mypage1',function (req,res){
 app.get('/wbp1',function (req,res){
    res.sendFile(path.join(__dirname,'ui','wbp1.html'));
 });
+app.get('/ui/style.css',function (req,res){
+    res.sendFile(path.join(__dirname,'ui','style.css'));
+});
 app.get('/ui/main.js',function (req,res){
     res.sendFile(path.join(__dirname,'ui','main.js'));
 });
