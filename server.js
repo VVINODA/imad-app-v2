15f31a95d56871ac296cd8d364a9808ef1a5355d
VVINODA <vinoda05v@gmail.com>
2017-02-06 19:11:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,6 +25,11 @@ app.get('/ui/main.js',function (req,res){
  app.get('/ui/madi.png',function (req,res){
     res.sendFile(path.join(__dirname,'ui','madi.png'));
 });
+var counter = 0;
+app.get('counter',function (req,res){
+   counter= counter + 1;
+   res.send(counter,toString());
+});
 
 var port = 8080;
 app.listen(8080,function(){
