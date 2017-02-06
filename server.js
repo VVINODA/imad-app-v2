de9b07ca18091294b28e1e1b61e7ada4dc9882fa
VVINODA <vinoda05v@gmail.com>
2017-02-06 19:13:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/ui/main.js',function (req,res){
 var counter = 0;
 app.get('counter',function (req,res){
    counter= counter + 1;
-   res.send(counter,toString());
+   res.send(counter.toString());
 });
 
 var port = 8080;
