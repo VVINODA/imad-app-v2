50420250f0790a2b0126905309c5b958c190c730
VVINODA <vinoda05v@gmail.com>
2017-02-10 16:44:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/',function (req,res){
 });
 
 var names =[];
-app.get('/submit-name',function(req,res)//submit-name?name
+app.get('/submit-name',function(req,res)//submit-name?name=
 {
     var name =req.query.name;
     names.push(name);
