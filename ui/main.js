ec82fc35c87d7e06fbde262fdc6400c9aab4e151
VVINODA <vinoda05v@gmail.com>
2017-02-16 16:19:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,27 +26,29 @@ request1.send(null);
 
 
 var submit= document.getElementById('submit-btn');
+
 submit.onclick = function()
-{
-    var request= new XMLHttpRequest();
-    request.onreadystatechange = function()
-{
-    if (request.readyState ===  XMLHttpRequest.DONE)
     {
+             var request= new XMLHttpRequest();
+             
+             request.onreadystatechange = function()
+                {
+             if (request.readyState ===  XMLHttpRequest.DONE)
+             {
         //ACTION
-    if (request.status === 200)
-    {
+            if (request.status === 200)
+              {
         
-    var names= request.responseText;
-    names= JSON.parse(names);
+               var names= request.responseText;
+               names= JSON.parse(names);
    
-    var list ='';
-    for (var i=0; i< names.length; i++)
-        {
-    list += '<li>' + names[i] +'</li>';
-        }
-var ul = document.getElementById('namelist');
-ul.InnerHTML = list;
+               var list ='';
+               for (var i=0; i< names.length; i++)
+                       {
+                        list += '<li>' + names[i] +'</li>';
+                       }
+               var ul = document.getElementById('namelist');
+               ul.InnerHTML = list;
 }
 }
 
