d8ebad86dc438ce6515ed81dd22dfb7361e63355
VVINODA <vinoda05v@gmail.com>
2017-02-19 15:22:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,16 +12,13 @@ request.onreadystatechange = function()
     {
     if (request.status === 200)
     {
-  /*  var counter = request.responseText;
+    var counter = request.responseText;
     var span = document.getElementById('count');
-  span.innerHTML = counter.toString();*/
+  span.innerHTML = counter.toString();
     }
     }
     // not done
 };
- var counter = request.responseText;
-    var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
 
 request.open('GET','http://vvinoda.imad.hasura-app.io/counter', true);
 request.send(null);
