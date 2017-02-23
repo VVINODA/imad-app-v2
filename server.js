8c46b74217ff33c3d953d24de1a5289b11ec1155
VVINODA <vinoda05v@gmail.com>
2017-02-23 11:36:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ return htmlTemplate;
 }
 app.get('/:articleName',function (req,res){
     var articleName = req.params.articleName;
-   res.send(createTemplate(articles[articleName]));
+   res.send(htmlTemplate(articles[articleName]));
 });
 
 /*var counter = 0;
