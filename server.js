ba620465aefc2e96367d168225505937146e3f56
VVINODA <vinoda05v@gmail.com>
2017-02-25 13:23:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,9 +87,23 @@ var htmlTemplate =`
 return htmlTemplate;
 }
 
-app.get('/:articleName',function (req,res){
-    var articleName = req.params.articleName;
-   res.send(createTemplate(articles[articleName]));
+app.get('/articles/:articleName',function (req,res){
+pool.query("SELECT * FROM articles WHERE title" = + res.params.articleName ,function (err,result){
+
+    if (err){
+        res.status(500).send(err.toString());
+    }
+    else {
+       if(result.rows.length === 0) {
+           res.status(404).send('ARTICLE NOT FOUND');
+       }
+    } else 
+    {
+        var articleData = result.rows[0];
+     res.send(createTemplate(articleData));
+    } 
+}); 
+   
 });
 
 
