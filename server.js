ce5f8b716206e74cec01d13b14a136276df7b4ac
VVINODA <vinoda05v@gmail.com>
2017-02-25 16:07:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ return htmlTemplate;
 }
 
 app.get('/articles/:articleName',function (req,res){
-pool.query("SELECT * FROM articles WHERE title = '" + req.params.articleName + '"', function (err,result){
+pool.query("SELECT * FROM articles WHERE title = $1" [req.params.articleName], function (err,result){
 
     if (err){
         res.status(500).send(err.toString());
